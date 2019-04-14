7d073f95e1de5630142c99cb773a221513cd8578
stobin <stobin@stobin.net>
2019-04-14 02:16:18 +0200

    Add rangeSlider plugin to HourPicker class

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -1,8 +1,44 @@
-import { BaseWidget } from "./BaseWidget.js";
-import { settings } from "../settings.js";
+/* global rangeSlider */
+
+import { BaseWidget } from './BaseWidget.js';
+import { select, settings } from '../settings.js';
+import { utils } from '../utils.js';
 
 export class HourPicker extends BaseWidget {
   constructor(wrapper) {
     super(wrapper, settings.hours.open);
+    const thisWidget = this;
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
+    thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
+
+    thisWidget.initPlugin();
+    thisWidget.value = thisWidget.dom.input.value;
+  }
+
+  initPlugin() {
+    const thisWidget = this;
+
+    /* [NEW] Add rangeSlider plugin */
+    rangeSlider.create(thisWidget.dom.input);
+
+    thisWidget.dom.input.addEventListener('input', function() {
+      thisWidget.value = thisWidget.dom.input.value;
+    });
+  }
+
+  parseValue(newValue) {
+    return utils.numberToHour(newValue);
+  }
+
+  isValid() {
+    return true;
+  }
+
+  renderValue() {
+    const thisWidget = this;
+
+    thisWidget.dom.output.innerHTML = thisWidget.value;
+    console.log('renderValue', thisWidget.value);
   }
 }
